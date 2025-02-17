1fc8a492e5b21aed0ffbd9005ade81e79ad26f74
Scott DePouw <scott.depouw@nimblepros.com>
2025-02-17 14:50:44 -0500

    .editorconfig and dotnet format

--- src/RiverBooks.Web/Program.cs
+++ src/RiverBooks.Web/Program.cs
@@ -1,35 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+  app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+string[] summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+  WeatherForecast[] forecast = Enumerable.Range(1, 5).Select(index =>
+    new WeatherForecast
+    (
+      DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+      Random.Shared.Next(-20, 55),
+      summaries[Random.Shared.Next(summaries.Length)]
+    )
+  ).ToArray();
+  return forecast;
 })
 .WithName("GetWeatherForecast");
 
@@ -37,5 +37,5 @@ app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
