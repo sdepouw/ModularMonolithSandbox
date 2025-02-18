1827a809f30731b42feb2d7de9f89c28cbdcb951
Scott DePouw <scott.depouw@nimblepros.com>
2025-02-18 12:25:19 -0500

    Price request validator

---
+++ src/RiverBooks.Books/BookEndpoints/UpdatePriceRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace RiverBooks.Books.BookEndpoints;
+
+internal class UpdatePriceRequestValidator : Validator<UpdatePriceRequest>
+{
+  public UpdatePriceRequestValidator()
+  {
+    RuleFor(x => x.Id)
+      .NotNull()
+      .NotEqual(Guid.Empty)
+      .WithMessage("A book ID is required.");
+
+    RuleFor(x => x.NewPrice)
+      .GreaterThanOrEqualTo(0m)
+      .WithMessage("Book prices may not be negative.");
+  }
+}
