8a01aecf977d02d01672c4bcd25f442dd5e250c2
Scott DePouw <scott.depouw@nimblepros.com>
2025-02-18 10:01:02 -0500

    Modeling the Books Domain

---
+++ src/RiverBooks.Books/Book.cs
@@ -0,0 +1,21 @@
+﻿using Ardalis.GuardClauses;
+
+namespace RiverBooks.Books;
+
+internal class Book
+{
+  public Guid Id { get; private set; }
+  public string Title { get; private set; }
+  public string Author { get; private set; }
+  public decimal Price { get; set; }
+
+  internal Book(Guid id, string title, string author, decimal price)
+  {
+    Id = Guard.Against.Default(id);
+    Title = Guard.Against.NullOrWhiteSpace(title);
+    Author = Guard.Against.NullOrWhiteSpace(author);
+    Price = Guard.Against.Negative(price);
+  }
+
+  internal void UpdatePrice(decimal newPrice) => Price = Guard.Against.Negative(newPrice);
+}
