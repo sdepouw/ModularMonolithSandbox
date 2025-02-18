ab215820c89176e4666e6de979679abe26d091c9
Scott DePouw <scott.depouw@nimblepros.com>
2025-02-18 14:36:14 -0500

    Getting book details when adding to Cart

---
+++ src/RiverBooks.Books/Contracts/BookDetailsQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Result;
+using MediatR;
+
+namespace RiverBooks.Books.Contracts;
+
+internal class BookDetailsQueryHandler(IBookService bookService) : IRequestHandler<BookDetailsQuery, Result<BookDetailsResponse>>
+{
+  public async Task<Result<BookDetailsResponse>> Handle(BookDetailsQuery request, CancellationToken cancellationToken)
+  {
+    BookDTO? book = await bookService.GetBookByIdAsync(request.BookId);
+    return book is not null
+      ? new BookDetailsResponse(book.Id, book.Title, book.Author, book.Price)
+      : Result.NotFound();
+  }
+}
--- src/RiverBooks.Users/UseCases/AddItemToCartHandler.cs
+++ src/RiverBooks.Users/UseCases/AddItemToCartHandler.cs
@@ -1,9 +1,11 @@
 ﻿using Ardalis.Result;
 using MediatR;
+using RiverBooks.Books.Contracts;
 
 namespace RiverBooks.Users.UseCases;
 
-internal class AddItemToCartHandler(IApplicationUserRepository userRepository) : IRequestHandler<AddItemToCartCommand, Result>
+internal class AddItemToCartHandler(IApplicationUserRepository userRepository, IMediator mediator)
+  : IRequestHandler<AddItemToCartCommand, Result>
 {
   public async Task<Result> Handle(AddItemToCartCommand request, CancellationToken cancellationToken)
   {
@@ -13,8 +15,15 @@ internal class AddItemToCartHandler(IApplicationUserRepository userRepository) :
       return Result.Unauthorized();
     }
 
-    // TODO: Get description and price form Books Module
-    CartItem newItem = new(request.BookId, "description", request.Quantity, 1.00m);
+    BookDetailsQuery query = new(request.BookId);
+    Result<BookDetailsResponse> bookDetailsResult = await mediator.Send(query, cancellationToken);
+    if (bookDetailsResult.IsNotFound())
+    {
+      return Result.NotFound();
+    }
+    BookDetailsResponse bookDetails = bookDetailsResult.Value;
+    string description = $"{bookDetails.Title} by {bookDetails.Author}";
+    CartItem newItem = new(request.BookId, description, request.Quantity, bookDetails.Price);
     user.AddItemToCart(newItem);
 
     await userRepository.SaveChangesAsync();
